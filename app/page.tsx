0a7f5d98eda00cd79ce67be79cc3d06ff4bb839b
Giovanni Nogueira Glins <106540123+Gio-Glins@users.noreply.github.com>
2025-05-14 12:56:16 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -244,7 +244,7 @@ export default function PlenariaApp() {
             </Card>
           )}
 
-          {usuario !== administrador && processoSelecionado == 0 &&(
+          {usuario !== administrador && processosRestantes.length === 0 && (
             <div className="mt-10 bg-white p-6 rounded shadow-md text-black">
               <h2 className="text-2xl font-bold mb-4 text-blue-800">
                 Resumo dos Votos (Para consulta do Administrador)
