be9de3f44e77091ea768fa18cd2f19434e167840
Giovanni Nogueira Glins <106540123+Gio-Glins@users.noreply.github.com>
2025-05-15 11:04:47 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -172,9 +172,9 @@ export default function PlenariaApp() {
           </p>
 
           {usuario !== administrador && processos.length > 0 && processosRestantes.length > 0 && (
-            <div className="flex-grid ">
+            <div className="inline-flex ">
               {processosRestantes.map((proc) => (
-                <Button key={proc.id} className="border px-1 p-1" onClick={() => selecionarProcesso(proc.id.toString())}>
+                <Button key={proc.id} className="border px-0.2 p-1" onClick={() => selecionarProcesso(proc.id.toString())}>
                   {proc.id} - {proc.numero}
                 </Button>
               ))}
