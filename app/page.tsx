dd2c878cf847c3eb9b7a1d455614f1342090bcc8
Giovanni Nogueira Glins <106540123+Gio-Glins@users.noreply.github.com>
2025-05-19 10:24:36 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -180,7 +180,7 @@ export default function PlenariaApp() {
   {processosRestantes.map((proc) => (
     <Button
       key={proc.id}
-      className="border px-4 py-2 bg-gray-950 hover:bg-gray-700"
+      className="border px-4 py-2 bg-gray-950 hover:bg-gray-700 hover:border-2"
       onClick={() => selecionarProcesso(proc.id.toString())}
     >
       {proc.id}
