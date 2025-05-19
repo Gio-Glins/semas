aec31a241169c62ea0a2f38443ca499995a91637
Giovanni Nogueira Glins <106540123+Gio-Glins@users.noreply.github.com>
2025-05-19 10:31:40 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -148,7 +148,7 @@ export default function PlenariaApp() {
       {!planilhaCarregada ? (
         <div className="space-y-2 max-w-2xl bg-gray-900 rounded p-4 mx-auto">
           <label className="block mb-2 font-semibold text-lg">Carregar planilha Excel:</label>
-          <input type="file" accept=".xlsx, .xls" onChange={handleUpload} className="p-1 border rounded w-full bg-gray-700 text-white" />
+          <input type="file" accept=".xlsx, .xls" onChange={handleUpload} className="p-1 border rounded w-full bg-gray-700 text-gray-700" />
         </div>
       ) : !autenticado ? (
         <div className="space-y-4 max-w-md bg-gray-800 shadow-md rounded p-6 mx-auto">
