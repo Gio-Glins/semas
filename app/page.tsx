7dd8f0eee2fc0036fd695b1ad642f431f0e6520d
Giovanni Nogueira Glins <106540123+Gio-Glins@users.noreply.github.com>
2025-07-30 11:29:18 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -141,6 +141,7 @@ export default function PlenariaApp() {
   };
 
   return (
+    <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
